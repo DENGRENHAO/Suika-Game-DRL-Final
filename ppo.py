af7b3427f8d848bf66619d32d704fccb8738100a
b10902118 <b10902118@linux1.csie.ntu.edu.tw>
2025-05-25 11:20:55 +0800

    try ppo copied from sac.

--- ppo.py
+++ ppo.py
@@ -4,9 +4,25 @@ import gymnasium as gym
 from gymnasium import spaces
 from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
 from stable_baselines3 import PPO
+from stable_baselines3.common.monitor import Monitor
+from stable_baselines3.common.vec_env import DummyVecEnv, VecVideoRecorder
+from wandb.integration.sb3 import WandbCallback
+from stable_baselines3.common.callbacks import BaseCallback
+import wandb
 import torch
 import torch.nn as nn
 import numpy as np
+import datetime
+from collections import deque
+
+# import cv2
+
+config = {
+    "env_name": "suika-game-l1-v0",
+    "policy_type": "MultiInputPolicy",
+    "total_timesteps": 300000,
+    "batch_size": 256,
+}
 
 
 class MyCombinedExtractor(BaseFeaturesExtractor):
@@ -26,6 +42,10 @@ class MyCombinedExtractor(BaseFeaturesExtractor):
             nn.Flatten(),
         )
 
+        for layer in self.cnn:
+            if isinstance(layer, nn.Conv2d):
+                torch.nn.init.xavier_uniform_(layer.weight)
+
         # Compute CNN output dim
         with torch.no_grad():
             sample_frame = torch.zeros((1, *frames_shape[1:]))
@@ -34,14 +54,14 @@ class MyCombinedExtractor(BaseFeaturesExtractor):
         # Process sequence of frames
         self.frame_proj = nn.Sequential(
             nn.Flatten(start_dim=1),
-            nn.Linear(n_frames * cnn_out_dim, 256),
+            nn.Linear(n_frames * cnn_out_dim, 512),
             nn.ReLU(),
         )
 
         # Final projection
-        self.final = nn.Sequential(nn.Linear(256 + 1 + 1, 256), nn.ReLU())
+        self.final = nn.Sequential(nn.Linear(512 + 1 + 1, 512), nn.ReLU())
 
-        self._features_dim = 256
+        self._features_dim = 512
 
     def forward(self, observations):
         frames = observations["boards"].float() / 255.0  # normalize to [0,1]
@@ -58,9 +78,9 @@ class MyCombinedExtractor(BaseFeaturesExtractor):
             observations["cur_fruit"] = observations["cur_fruit"].unsqueeze(0)
             observations["next_fruit"] = observations["next_fruit"].unsqueeze(0)
         cur_fruit = (
-            torch.argmax(observations["cur_fruit"], dim=-1)
-        ) + 1  # +1 to align with gray values
-        next_fruit = torch.argmax(observations["next_fruit"], dim=-1) + 1
+            torch.argmax(observations["cur_fruit"], dim=-1) + 1
+        ) / 5  # +1 to align with gray values, and normalize to [0,1]
+        next_fruit = (torch.argmax(observations["next_fruit"], dim=-1) + 1) / 5
 
         return self.final(
             torch.cat(
@@ -74,13 +94,86 @@ class MyCombinedExtractor(BaseFeaturesExtractor):
         )
 
 
-env = CoordSizeToImage(env=SuikaEnv())
+class WandbLoggingCallback(BaseCallback):
+    def __init__(self, eval_env, log_interval=500, verbose=0):
+        super().__init__(verbose)
+        self.eval_env = eval_env
+        self.interval = log_interval
+        self.eval_interval = log_interval * 10
+        self.ep_reward = 0
+        self.rewards = deque(maxlen=100)
+        self.scores = deque(maxlen=100)
+
+    def _on_step(self) -> bool:
+        def evaluate():
+            env = self.eval_env
+            obs, info = env.reset()
+            done = False
+            frames = []
+            while not done:
+                action, _ = self.model.predict(obs, deterministic=True)
+                obs, reward, done, truncated, info = env.step(action)
+                frames += env.unwrapped.render_states(states=info["fruit_states"])
+            return info["score"], frames
+
+        self.ep_reward += self.locals["rewards"][0]
+        if self.locals["dones"][0]:
+            self.rewards.append(self.ep_reward)
+            self.scores.append(self.locals["infos"][0]["score"])
+            self.ep_reward = 0
+
+        if (self.num_timesteps + 1) % self.interval == 0:
+            logs = {
+                "train_reward": np.mean(self.rewards) if len(self.rewards) > 0 else 0,
+                "train_scores": np.mean(self.scores) if len(self.scores) > 0 else 0,
+            }
+            if (self.num_timesteps + 1) % self.eval_interval == 0:
+                score, frames = evaluate()
+                print(f"Step {self.num_timesteps} eval score: {score:.2f}")
+                frames = np.array(frames)
+                frames = frames.transpose(0, 3, 1, 2)  # Convert to (T, C, H, W) formath
+                logs["eval_score"] = score
+                logs["video"] = wandb.Video(
+                    frames,
+                    fps=30,
+                    format="mp4",
+                )
+            wandb.log(logs, step=self.num_timesteps)
+        return True
+
+
+run = wandb.init(
+    project="suika-sb3-ppo",
+    name=datetime.datetime.now().strftime("%m-%d_%H-%M"),
+    config=config,
+    settings=wandb.Settings(x_disable_stats=True),
+)
+
+
+def make_env():
+    env = gym.make(config["env_name"], render_mode="rgb_array")
+    env = CoordSizeToImage(env=env)
+    return env
+
+
+env = make_env()
 
 policy_kwargs = dict(
     features_extractor_class=MyCombinedExtractor,
-    # features_extractor_kwargs={"observation_space": env.observation_space},
 )
 
-model = PPO("MultiInputPolicy", env, policy_kwargs=policy_kwargs, verbose=1)
-model.learn(total_timesteps=100_000)
-model.save("ppo_model")
+model = PPO(
+    config["policy_type"],
+    env,
+    policy_kwargs=policy_kwargs,
+    verbose=1,
+    batch_size=config["batch_size"],
+)
+model.learn(
+    total_timesteps=config["total_timesteps"],
+    log_interval=1,  # episode
+    progress_bar=True,
+    callback=WandbLoggingCallback(make_env()),
+)
+# model.save("sac_model")
+run.finish()
